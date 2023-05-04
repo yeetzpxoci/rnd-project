87306dee939680d6adf6303627f93c824a1a2312
falowanablaszka <daniel.furmanczyk@o2.pl>
2023-05-04 12:52:56 +0200

    terrific

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { game } from './gameLogic.js'
 const dom = DOM();
 const newGame = game();
 
-newGame.startGame();
+newGame.startGame(1000);
 
 document.getElementById('start-button').addEventListener('click', dom.resetPage);
 document.getElementById('difficulty-button').addEventListener('click', dom.renderStartPage);
